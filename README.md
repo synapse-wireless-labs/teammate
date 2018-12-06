e9038f1872ceccd4624af0d0681b52b1986acdd7
Mike Ryan <mike.ryan52@gmail.com>
2018-12-06 10:01:08 -0600

    Scaffolded out the Electron app

--- README.md
+++ README.md
@@ -13,3 +13,18 @@ To start the project, run `yarn start:msft-graph-connector` from the root of thi
 Connector library to get pull requests and build statuses for a user's repos
 
 To start the project, run `yarn start:github-graph-connector` from the root of this project. Sources are in `./projects/github-graph-connector`.
+
+#### Desktop App
+
+App used to connect to and configure a Teammate device. To run this project, first you have to start the UI development server:
+
+```sh
+cd ./projects/desktop-ui
+yarn start
+```
+
+Then start the Electron app from the root of the repo:
+
+```sh
+yarn start:desktop
+```
---
+++ projects/desktop-backend/src/main.ts
@@ -0,0 +1,41 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+import * as url from "url";
+
+let win: BrowserWindow | null = null;
+
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 });
+
+  // load the dist folder from Angular
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `../../desktop-ui/dist/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  // The following is optional and will open the DevTools:
+  // win.webContents.openDevTools()
+
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+// on macOS, closing the window doesn't quit the app
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+// initialize the app's main window
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
